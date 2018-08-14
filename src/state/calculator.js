8a09c67598209603fbe1a8eae63264bf72bd1ab6
Kacper Swietlaga <kacperres@gmail.com>
2018-08-14 15:21:03 +0200

    added new adding test

--- src/state/calculator.js
+++ src/state/calculator.js
@@ -3,30 +3,43 @@ const INPUT = 'calculator/INPUT'
 const ADD = 'calaculator/ADD'
 const SUBSTRATION = 'calaculator/SUBSTRATION'
 
+export const addAction = () => (
+    {
+        type: ADD
+    }
+)
+
 export const inputAction = (number) => (
     {
         type: INPUT,
         number
     }
 )
-export const addAction =() =>({
-    type: ADD
-})
+
 
 const initialState = {
     result: 0,
     input: 0,
-    isResultsIsShown: false 
+    isResultShown: false,
+    lastOperation: null,
 }
-export default (state= initialState, action) => {
-    switch(action.type) {
-        case INPUT: 
-        return {
-            ...state,
-            input:state.input * 10 +  action.number ,
-            isResultsIsShown: false 
-        }
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case INPUT:
+            return {
+                ...state,
+                input: state.input * 10 + action.number,
+                isResultShown: false
+            }
+        case ADD:
+            return {
+                ...state,
+                input: 0,
+                lastOperation: action.type,
+                isResultShown: true,
+                result: state.result + state.input,
+            }
         default:
-        return state
+            return state
     }
 }
